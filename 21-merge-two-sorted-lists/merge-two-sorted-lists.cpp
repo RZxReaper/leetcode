cd29609c6283d58eb3ef29faa9f8d5898a8ebf50
RZxReaper <86017539+RZxReaper@users.noreply.github.com>
2024-09-24 23:15:12 +0530

    Time: 3 ms (85.87%) | Memory: 19.6 MB (96.57%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -18,13 +18,32 @@ public:
         if (list2 == NULL){
             return list1;
         }
-        if(list1->val <= list2 -> val){
-            list1->next=mergeTwoLists(list1->next,list2);
-            return list1;
+        if(list1->val<=list2->val){
+            res=list1;
+            list1=list1->next;
         }
         else{
-            list2->next=mergeTwoLists(list1,list2->next);
-            return list2;
+            res=list2;
+            list2=list2->next;
         }
+        ListNode* head = res;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val <= list2->val){
+                res->next=list1;
+                list1=list1->next;
+            }
+            else{
+                res->next=list2;
+                list2=list2->next;
+            }
+            res=res->next;
+        }
+        if(list1==NULL){
+            res->next=list2;
+        }
+        if (list2==NULL){
+            res->next=list1;
+        }
+        return head;
     }
 };
