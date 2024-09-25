78b1531f8d9884fcebd065b33850dfdebb5586d0
RZxReaper <86017539+RZxReaper@users.noreply.github.com>
2024-09-26 00:33:34 +0530

    Time: 40 ms (23.20%) | Memory: 29.1 MB (17.71%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     vector<string> sortPeople(vector<string>& n, vector<int>& h) {
         unordered_map<int,string> u;
-        vector<string> res;
         if(n.size()==1){return n;}
         for(int i=0;i<h.size();i++){
             u[h[i]]=n[i];
         }
-        sort(h.begin(),h.end());
-        for(int i=h.size()-1;i>=0;i--){
-            res.push_back(u[h[i]]);
+        sort(h.rbegin(),h.rend());
+        for(int i=0;i<h.size();i++){
+            n[i]=u[h[i]];
         }
-        return res;
+        return n;
     }
 };
