e11cf5b6a3a44306397eac43ce22f24a1a943c5d
RZxReaper <86017539+RZxReaper@users.noreply.github.com>
2024-09-25 23:14:22 +0530

    Time: 38 ms (47.11%) | Memory: 11.7 MB (89.72%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.py
+++ 728-self-dividing-numbers/self-dividing-numbers.py
@@ -8,9 +8,8 @@ class Solution(object):
         res=[]
         flag=1
         for i in range(left,right+1):
-            st = str(i)
             flag = 1
-            for j in st:
+            for j in str(i):
                 if (int(j)==0 or i%int(j)!=0):
                     flag = 0
                     break
